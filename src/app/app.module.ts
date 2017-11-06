7b0911c164fb98a59bef51c53178c2d8b35f2fe0
Konstantin Kharitonov <kharitonov.k.y@gmail.com>
2017-11-06 23:22:04 +0300

    updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-import { DataService } from "./services/data.service";
-import { DbService } from "./services/db.service";
+import { DbService } from './services/db.service';
 import { PopupComponent } from './popup/popup.component';
-import { StoreService } from "./services/store.service";
+import { StoreService } from './services/store.service';
 import { SectionIndexComponent } from './section-index/section-index.component';
 import { SectionGroupComponent } from './section-group/section-group.component';
 import { SectionCardComponent } from './section-card/section-card.component';
-import { RouterModule } from "@angular/router";
-import { rootRoutes } from "./routers/root-routers";
+import { RouterModule } from '@angular/router';
+import { rootRoutes } from './routers/root-routers';
 import { PopupGroupComponent } from './popup-group/popup-group.component';
 
 @NgModule({
@@ -28,10 +26,9 @@ import { PopupGroupComponent } from './popup-group/popup-group.component';
     RouterModule.forRoot(rootRoutes, { useHash: true }),
     BrowserModule,
     FormsModule,
-    ReactiveFormsModule,
-    HttpModule
+    ReactiveFormsModule
   ],
-  providers: [ DbService, DataService, StoreService ],
+  providers: [ DbService, StoreService ],
   entryComponents: [ PopupComponent, PopupGroupComponent ],
   bootstrap: [ AppComponent ]
 })
--- src/app/services/data.service.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable } from '@angular/core';
-import { DbService } from './db.service';
-
-@Injectable()
-export class DataService {
-  constructor(private _dbService: DbService) {
-
-  }
-}
